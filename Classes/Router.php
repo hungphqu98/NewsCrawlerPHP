b1f573012e6db5be1be3b0f80c8a71d4f8137243
hungphqu98 <pqh2405@gmail.com>
2021-07-23 15:08:01 +0700

    Remove whitespace in dantri data

--- Classes/Router.php
+++ Classes/Router.php
@@ -13,23 +13,17 @@ class Router {
     
   }
 
-  // Route action based on sites
+  // Route action based on URL
   function routing() {
 
     $url = $this->curl->getInfo()["url"];
 
      if (preg_match('/dantri/',$url)) {
-        $parse = new dtParser($this->curl);
-        $array = $parse->printParse();
-        return $array;
+        $parser = new dtParser($this->curl);
       } else if (preg_match('/vnexpress/',$url)) {
-        $parse = new vneParser($this->curl);
-        $array = $parse->printParse();
-        return $array;
+        $parser = new vneParser($this->curl);
       } else if (preg_match('/vietnamnet/',$url)) {
-        $parse = new vnnParser($this->curl);
-        $array = $parse->printParse();
-        return $array;
+        $parser = new vnnParser($this->curl);
       } else if (empty($url)){
         echo '<pre>';
         echo "No URl sent";
@@ -39,6 +33,9 @@ class Router {
         echo "Only URLs from dantri,vietnamnet,vnexpress are allowed";
         echo '</pre>';
       }
+
+      $result = $parser->printParse();
+      return $result;
   }
 
 }
--- Classes/SiteParser/dtParser.php
+++ Classes/SiteParser/dtParser.php
@@ -17,7 +17,9 @@
     public function printTitle($news) {
 
       $q = $news->query("//*[@class='dt-news__title']");
-      $title = $q->item(0)->nodeValue;
+      $titles = $q->item(0)->nodeValue;
+      $titles = preg_replace('/\s+/', ' ', $titles);
+      $title = trim($titles," ");
       echo 'Title: '. $title;
       return $title;
 
@@ -26,9 +28,12 @@
     // Print content
     public function printContent($news) {
 
-      $q = $news->query("//*[@class='dt-news__content']");
-      $content = $q->item(0)->nodeValue;
-      echo 'Content: '.$content;
+      $q = $news->query("//*[@class='dt-news__content']/p");
+      $content = '';
+      foreach ($q as $s) {
+        $content .= $s->nodeValue;
+      }
+      echo 'Content:' . $content;
       return $content;
 
     }
