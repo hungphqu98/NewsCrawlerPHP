b19ba52c8e65a2b33ef6ad03e5bca21d2449e905
hungphqu98 <55435606+hungphqu98@users.noreply.github.com>
2021-07-29 14:04:54 +0700

    rework site parsers

--- Classes/SubParser/PageParser.php
+++ Classes/SubParser/PageParser.php
@@ -4,53 +4,88 @@ abstract class PageParser {
 
   protected $curl;
 
-  public function __construct(Curl $curl) {
+  public $title = '';
 
-    $this->curl = $curl;
-    if(!$curl){
-      throw new \Exception('Can not construct parser');
+  public $content = '';
+
+  public $date = '';
+
+    public function __construct(Curl $curl) {
+
+      $this->curl = $curl;
+      if(!$curl){
+        throw new \Exception('Can not construct parser');
+      }
+
     }
 
-  }
+    // Print parsed data 
+    public function getParse() {
 
-   // Print parsed data 
-   public function getParse() {
+        $news = $this->htmlParse();
+        $title = $this->getTitle($news);
+        $content = $this->getContent($news);
+        $date = $this->getDate($news);
 
-      $news = $this->htmlParse();
-      $title = $this->getTitle($news);
-      echo '<br>';
-      $content = $this->getContent($news);
-      echo '<br>';
-      $date = $this->getDate($news);
-      $arr = array(
-        'title' => $title,
-        'content' => $content,
-        'date' => $date
-      );
-      return $arr;
-  }
+        $arr = array(
+          'title' => $title,
+          'content' => $content,
+          'date' => $date
+        );
+        return $arr;
+    }
 
-  // Parse page data from html
-  public function htmlParse() {
- 
-    $html = $this->curl->exec();
-    $dom = new DOMDocument();
-    $dom->loadHTML('<?xml encoding="utf-8" ?>' . $html,LIBXML_NOERROR);
-    $parse = new DOMXPath($dom);
-    return $parse;
-    
-  }
+    // Parse page data from html
+    public function htmlParse() {
+  
+      $html = $this->curl->exec();
+      $dom = new DOMDocument();
+      $dom->loadHTML('<?xml encoding="utf-8" ?>' . $html,LIBXML_NOERROR);
+      $parse = new DOMXPath($dom);
+      return $parse;
+      
+    }
 
-  // get title
-  abstract public function getTitle($title);
+    // get title
+    abstract protected function getTitle($title);
 
-  // get content
-  abstract public function getContent($content);
+    // get content
+    abstract protected function getContent($content);
 
-  // get published date
-  abstract public function getDate($date);
+    // get published date
+    abstract protected function getDate($date);
 
+    // format title 
+    protected function formatTitle($data) {
 
+      $titles = $data->item(0)->nodeValue;
+      $titles = preg_replace('/\s+/', ' ', $titles);
+      $title = trim($titles," ");
+      return $title;
+
+    }
+
+    // format content
+    protected function formatContent($data) {
+      
+      $content = '';
+      foreach ($data as $line) {
+        $content .= $line->nodeValue;
+      }
+      return $content;
+
+    }
+
+    // format date 
+    protected function formatDate($data) {
+
+      $dateString = $data->item(0)->nodeValue;
+      preg_match('^\\d{1,2}/\\d{1,2}/\\d{4}^',$dateString,$day);
+      preg_match('^\\d{1,2}:\\d{1,2}^',$dateString,$time);
+      $date = $day[0]. " " .$time[0];
+      return $date;
+
+    }
 
 
 }
