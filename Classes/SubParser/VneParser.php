ce8ab475e3c1c8d9e658349152a00bd1928ae243
hungphqu98 <55435606+hungphqu98@users.noreply.github.com>
2021-07-29 14:05:28 +0700

    rework site parsers

--- Classes/SubParser/VneParser.php
+++ Classes/SubParser/VneParser.php
@@ -1,40 +1,32 @@
 <?php 
   
   class VneParser extends PageParser {
+    
+    // get title
+    protected function getTitle($news) {
+  
+      $data = $news->query("//*[@class='title-detail']");
+      $title = $this->formatTitle($data);
+      return $this->title = $title;
+
+    }
  
-   // get title
-   public function getTitle($news) {
- 
-     $q = $news->query("//*[@class='title-detail']");
-     $title = $q->item(0)->nodeValue;
-     echo 'Title: '. $title;
-     return $title;
- 
-   }
- 
-   // get content
-   public function getContent($news) {
- 
-     $q = $news->query("//*[@class='Normal']");
-     $content = '';
-     foreach ($q as $s) {
-       $content .= $s->nodeValue;
-     }
-     echo 'Content:' . $content;
-     return $content;
-     
-   }
- 
-   // get published date
-   public function getDate($news) {
- 
-     $q = $news->query("//*[@class='date']");
-     $dateString = $q->item(0)->nodeValue;
-     preg_match('^\\d{1,2}/\\d{1,2}/\\d{4}^',$dateString,$day);
-     preg_match('^\\d{1,2}:\\d{1,2}^',$dateString,$time);
-     $date = $day[0]. " " .$time[0];
-     echo 'Date: '.$date;
-     return $date;
-   }
+    // get content
+    protected function getContent($news) {
+  
+      $data = $news->query("//*[@class='Normal']");
+      $content = $this->formatContent($data);
+      return $this->content = $content;
+      
+    }
+  
+    // get published date
+    protected function getDate($news) {
+  
+      $data = $news->query("//*[@class='date']");
+      $date = $this->formatDate($data);
+      return $this->date = $date;
+
+    }
  
  }
