607db6bf31e20ab9b8c987b4df98cb7ff3d81e41
hungphqu98 <55435606+hungphqu98@users.noreply.github.com>
2021-07-29 14:04:28 +0700

    Rework site parsers

--- Classes/SubParser/DtParser.php
+++ Classes/SubParser/DtParser.php
@@ -3,40 +3,29 @@
   class DtParser extends PageParser {
 
     // get title
-    public function getTitle($news) {
+    protected function getTitle($news) {
 
-      $q = $news->query("//*[@class='dt-news__title']");
-      $titles = $q->item(0)->nodeValue;
-      $titles = preg_replace('/\s+/', ' ', $titles);
-      $title = trim($titles," ");
-      echo 'Title: '. $title;
-      return $title;
+      $data = $news->query("//*[@class='dt-news__title']");
+      $title = $this->formatTitle($data);
+      return $this->title = $title;
 
     }
 
     // get content
-    public function getContent($news) {
+    protected function getContent($news) {
 
-      $q = $news->query("//*[@class='dt-news__content']/p");
-      $content = '';
-      foreach ($q as $s) {
-        $content .= $s->nodeValue;
-      }
-      echo 'Content:' . $content;
-      return $content;
+      $data = $news->query("//*[@class='dt-news__content']/p");
+      $content = $this->formatContent($data);
+      return $this->content = $content;
 
     }
 
     // get published date
-    public function getDate($news) {
+    protected function getDate($news) {
 
-      $q = $news->query("//*[@class='dt-news__time']");
-      $dateString = $q->item(0)->nodeValue;
-      preg_match('^\\d{1,2}/\\d{1,2}/\\d{4}^',$dateString,$day);
-      preg_match('^\\d{1,2}:\\d{1,2}^',$dateString,$time);
-      $date = $day[0]. " " .$time[0];
-      echo 'Date: '.$date;
-      return $date;
+      $data = $news->query("//*[@class='dt-news__time']");
+      $date = $this->formatDate($data);
+      return $this->date = $date;
       
     }
 
